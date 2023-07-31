52389f99bc96f7fdec669c46408d7b15431d1c4d
unknown <vikas@qualimatrix.in>
2023-07-31 18:48:01 +0530

    testing

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-// // index.js
-// require('dotenv').config();
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const app = express();
-// const db = require('./db');
-// const blogRoutes = require('./routes/blogs');
-// const formRoutes = require('./routes/forms');
-// const authRoutes = require('./routes/auth');
+// index.js
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//const db = require('./db');
+const blogRoutes = require('./routes/blogs');
+const formRoutes = require('./routes/forms');
+const authRoutes = require('./routes/auth');
 
-// // Middlewares
-// app.use(bodyParser.json());
-// app.use(cors());
+// Middlewares
+app.use(bodyParser.json());
+app.use(cors());
 
-// // Use the routes
-// app.use('/api', blogRoutes);
-// app.use('/api', formRoutes);
-// app.use('/api/auth', authRoutes);
-// app.get("/test", function (req, res) {
-//     res.send("Testing Vicky")
-// });
+// Use the routes
+app.use('/api', blogRoutes);
+app.use('/api', formRoutes);
+app.use('/api/auth', authRoutes);
+app.get("/hell", function (req, res) {
+    res.send("helllllll")
+});
 
-// // Start server
+// Start server
 // const port = process.env.PORT || 5000;
 // app.listen(port, () => {
 //     console.log(`Server is running on http://localhost:${port}`);
