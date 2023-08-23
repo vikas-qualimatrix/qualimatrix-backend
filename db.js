431d8a589d030956e6037a2c37174e74c2c31d97
vikas-narwariya <vickylodhi05@gmail.com>
2023-08-23 20:23:44 +0530

    code refactor

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 // db.js
-const mongoose = require('mongoose');
+//require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://vikas:qualimatrix@qualimatrix.6irk4ri.mongodb.net/qualimatrix-backend", {
+mongoose.connect(
+  "mongodb+srv://vikas:qualimatrix@qualimatrix.6irk4ri.mongodb.net/qualimatrix-backend",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+  }
+);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB successfully!');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB successfully!");
 });
